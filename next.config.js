430a95bdd14c5a398760bb3ddabbc52a0d044052
Tigran <41443306+septoon@users.noreply.github.com>
2023-01-17 00:01:40 +0300

    prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  poweredByHeader: false,
+  optimizeFonts: false,
+  env: {
+    
+  }
 }
 
 module.exports = nextConfig
